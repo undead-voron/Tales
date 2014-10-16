51b3e6e46b3409f0fce1d5fd8efec12a00e97a98
voron <undeadvoron@gmail.com>
2014-10-16 16:02:10 +0000

    overflow fixed

--- TalesMenu.js
+++ TalesMenu.js
@@ -3,68 +3,7 @@
 //Settings for Menu with Tales
 //
 /////////////////////////////////
-function bottomMenu(){
 
-  //getter for Tales menu
-  var bottomMenu = document.getElementsByClassName('bottomMenu');
-
-  //for every Tales menu
-  for (var imgCounter = 0;imgCounter<bottomMenu.length;imgCounter++){
-
-    //get parent
-    var imgParent = bottomMenu[imgCounter].parentNode;
-
-    //makes parent to show elements beyond it's borders
-    imgParent.style.overflowX = 'show';
-
-    //set background of a tales menu
-    var talesBackGround = document.createElement('div');
-
-    //background settings
-    talesBackGround.style.height = window.innerHeight + 'px';
-    talesBackGround.style.width = window.innerWidth + 'px';
-    talesBackGround.className = 'talesMenuBackground';
-    talesBackGround.style.left = ((window.innerWidth - imgParent.offsetWidth)/2) * (-1) + 'px';
-
-    //append background
-    imgParent.appendChild(talesBackGround);
-
-    //text getter
-    var picTextA = bottomMenu[imgCounter].getElementsByTagName('a');
-
-    //settings for text container
-    var picText = document.createElement('div');
-    picText.className = 'picText';
-
-    //text setter
-    for(var textCounter = 0; textCounter<picTextA.length;){
-      picText.appendChild(picTextA[0]);
-    }
-
-    //pictures getter
-    var bottomIMG = bottomMenu[imgCounter].getElementsByTagName('img');
-
-    //settings for text container
-    var picFrame = document.createElement('div');
-    picFrame.className = 'picFrame';
-    picFrame.appendChild(picText);
-    picFrame.style.height = (bottomIMG[0].offsetHeight * 2 + 50) + 'px';
-    picFrame.style.width = window.innerWidth * 0.9 + 'px';
-
-    for (var imageCounter=0;imageCounter<bottomIMG.length;){
-      picFrame.appendChild(bottomIMG[0]);
-    }
-
-    var mainPicFrame = document.createElement('div');
-    mainPicFrame.className = 'mainPicFrame';
-    mainPicFrame.style.width = window.innerWidth * 0.9 + 'px';
-    mainPicFrame.style.left = ((window.innerWidth - imgParent.offsetWidth)/2) * (-1) +( window.innerWidth * 0.05 )+ 'px';
-
-    mainPicFrame.appendChild(picFrame);
-
-    imgParent.appendChild(mainPicFrame);
-  }
-}
 
 ///////////////////////////////
 //
